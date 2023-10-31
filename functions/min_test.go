be15e1891516421312c0fc803bf9baf097ae78a6
Dennis Wethmar <denniswethmar@gmail.com>
2023-10-31 11:02:07 +0100

    Added test for sql functions

---
+++ functions/min_test.go
@@ -0,0 +1,91 @@
+package functions
+
+import (
+	"io"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/dwethmar/gosqle/expressions"
+)
+
+func TestMin_Write(t *testing.T) {
+	type fields struct {
+		Col *expressions.Column
+	}
+	type args struct {
+		sw io.StringWriter
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "write min",
+			fields: fields{
+				Col: &expressions.Column{
+					Name: "test",
+				},
+			},
+			args: args{
+				sw: new(strings.Builder),
+			},
+			want:    "MIN(test)",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Min{
+				Col: tt.fields.Col,
+			}
+
+			if err := m.Write(tt.args.sw); (err != nil) != tt.wantErr {
+				t.Errorf("Write() error = %v, wantErr %v", err, tt.wantErr)
+			} else if tt.want != "" {
+				if sb, ok := tt.args.sw.(*strings.Builder); ok {
+					if got := sb.String(); got != tt.want {
+						t.Errorf("Write() = %q, want %q", got, tt.want)
+					}
+				} else {
+					t.Errorf("expected string builder")
+				}
+			}
+		})
+	}
+}
+
+func TestNewMin(t *testing.T) {
+	type args struct {
+		column *expressions.Column
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Min
+	}{
+		{
+			name: "new min",
+			args: args{
+				column: &expressions.Column{
+					Name: "test",
+				},
+			},
+			want: &Min{
+				Col: &expressions.Column{
+					Name: "test",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewMin(tt.args.column); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewMin() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
